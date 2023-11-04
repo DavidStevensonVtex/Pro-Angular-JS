7a564c0776fabe5aefe96ac74faba9d4d6e09387
David Stevenson <drstevenson1958@yahoo.com>
2023-11-04 15:54:44 -0400

    LIsting 6-11. Adding filters to the customFilters.js file

--- ch06/SportsStore/Content/filters/customFilters.js
+++ ch06/SportsStore/Content/filters/customFilters.js
@@ -20,3 +20,32 @@
 			}
 		}
 	})
+	.filter("range", function ($filter) {
+		return function (data, page, size) {
+			if (angular.isArray(data) && angular.isNumber(page) && angular.isNumber(size)) {
+				let start_index = (page - 1) * size;
+				if (data.length < start_index) {
+					return [];
+				}
+				else {
+					return $filter("limitTo")(data.splice(start_index), size);
+				}
+			}
+			else {
+				return data;
+			}
+		}
+	})
+	.filter("pageCount", function () {
+		return function (data, size) {
+			if (angular.isArray(data)) {
+				let result = [];
+				for (var i = 0; i < Math.ceil(data.length / size); i++) {
+					result.push(i);
+				}
+			}
+			else {
+				return data;
+			}
+		}
+	});
