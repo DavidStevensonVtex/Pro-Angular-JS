899f97521f74972106a4549644ea855e90e429b5
David Stevenson <drstevenson1958@yahoo.com>
2023-11-04 14:04:57 -0400

    Create Product Repository

---
+++ ch06/SportsStore/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using SportsStore.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace SportsStore.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        //[Route("/products")]
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return Repository.Products;
+        }
+    }
+}
--- ch06/SportsStore/Models/Product.cs
+++ ch06/SportsStore/Models/Product.cs
@@ -2,6 +2,15 @@
 {
     public class Product
     {
+        public Product ( int id, string name, string description, string category, decimal price )
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Description = description;
+            this.Category = category;
+            this.Price = price;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
