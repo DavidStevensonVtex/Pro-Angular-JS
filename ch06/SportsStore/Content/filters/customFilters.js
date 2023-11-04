03d0e0b3668ff4e0fe6ea67a3592de73b8ef0a5e
David Stevenson <drstevenson1958@yahoo.com>
2023-11-04 14:48:17 -0400

    Listing 6-4. The Contents of the customFilters.js file

---
+++ ch06/SportsStore/Content/filters/customFilters.js
@@ -0,0 +1,29 @@
+﻿angular.module("customFilters", [])
+	.filter("unique", function () {
+		return function (data, propertyName) {
+			if (angular.isArray(data) && angular.isString(propertyName)) {
+				let results = [];
+				let keys = {};
+				//for (item of data) {
+				//	let val = data[propertyName];
+				//	if (angular.isUndefined(keys[val])) {
+				//		keys[val] = true;
+				//		results.push(val);
+				//	}
+				//}
+
+				for (let i = 0; i < data.length; i++) {
+					let val = data[i][propertyName];
+					if (angular.isUndefined(keys[val])) {
+						keys[val] = true;
+						results.push(val);
+					}
+				}
+
+				return results;
+			}
+			else {
+				return data;
+			}
+		}
+	})
