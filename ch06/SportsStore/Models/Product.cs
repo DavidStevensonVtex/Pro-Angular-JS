d647ca48d1dde2ed9119e9ac7c283446dae9356c
David Stevenson <drstevenson1958@yahoo.com>
2023-11-06 12:23:42 -0500

    Listing 7-1. Making an Ajax Request in the sportsStore.js file

--- ch06/SportsStore/Models/Product.cs
+++ ch06/SportsStore/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace SportsStore.Models
+﻿using Newtonsoft.Json;
+
+namespace SportsStore.Models
 {
     public class Product
     {
@@ -11,10 +13,19 @@
             this.Price = price;
         }
 
+        [JsonProperty("id")]
         public int Id { get; set; }
+
+        [JsonProperty("name")]
         public string Name { get; set; }
+
+        [JsonProperty("description")]
         public string Description { get; set; }
+
+        [JsonProperty("category")]
         public string Category { get; set; }
+
+        [JsonProperty("price")]
         public decimal Price { get; set; }
     }
 }
