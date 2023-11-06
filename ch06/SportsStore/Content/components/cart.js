0eca911066ddbd3e316235769269c0f7869495a5
David Stevenson <drstevenson1958@yahoo.com>
2023-11-06 13:34:05 -0500

    Listing 7-5. The Contents of the cart.js File

---
+++ ch06/SportsStore/Content/components/cart.js
@@ -0,0 +1,33 @@
+﻿angular.module("cart", [])
+	.factory("cart", function () {
+		let cartData = [];
+
+		return {
+			addProduct: function (id, name, price) {
+				let addedToExistingItem = false;
+				for (item of cartData) {
+					if (item.id == id) {
+						item.count++; addedToExistingItem = true;
+						break;
+					}
+				}
+
+				if (!addedToExistingITem) {
+					cartData.push({ count: 1, id: id, price: price, name: name });
+				}
+			},
+
+			removeProduct: function (id) {
+				for (let i = 0; i < cartData.length; i++) {
+					if (cartData[i].id == id) {
+						cartData.splice(i, 1);		// splice(start, deleteCount)
+						break;
+					}
+				}
+			},
+
+			getProducts: function () {
+				return cartData;
+			}
+		}
+	});
