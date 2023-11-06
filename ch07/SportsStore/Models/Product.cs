90b543a1c4b6ef53a1c54576d18cc47de57f8203
David Stevenson <drstevenson1958@yahoo.com>
2023-11-06 12:12:56 -0500

    Chapter 7: SportsStore: Navigation and Checkout

---
+++ ch07/SportsStore/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace SportsStore.Models
+{
+    public class Product
+    {
+        public Product ( int id, string name, string description, string category, decimal price )
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Description = description;
+            this.Category = category;
+            this.Price = price;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
