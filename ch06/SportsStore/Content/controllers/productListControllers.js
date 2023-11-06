2c3cc129eb36eda3b86e3526f2821682f91021bf
David Stevenson <drstevenson1958@yahoo.com>
2023-11-06 13:54:22 -0500

    Listing 7-9. Adding Support for the Cart to the productListControllers.js file

--- ch06/SportsStore/Content/controllers/productListControllers.js
+++ ch06/SportsStore/Content/controllers/productListControllers.js
@@ -1,7 +1,7 @@
 ﻿angular.module("sportsStore")
 	.constant("productListActiveClass", "btn-primary")
 	.constant("productListPageCount", 3)
-	.controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount) {
+	.controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount, cart) {
 		let selectedCategory = null;
 
 		$scope.selectedPage = 1;
@@ -27,4 +27,7 @@
 		$scope.getPageClass = function (page) {
 			return $scope.selectedPage == page ? productListActiveClass : "";
 		}
-	})
+		$scope.addProductToCart = function (product) {
+			cart.addProductToCart(product.id, product.name, product.price);
+		}
+	});
