e5c69c364e80c3114f51c59514df1deedbc8ee06
David Stevenson <drstevenson1958@yahoo.com>
2023-11-07 13:47:47 -0500

    Added the Order C# Class

---
+++ ch08/SportsStore/Models/Order.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsStore.Models
+{
+    public class Order
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("street")]
+        public string Street { get; set; }
+
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("zip")]
+        public string ZipCode { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("giftwrap")]
+        public bool GiftWrap { get; set; }
+
+        public List<Product> Products { get; set; }
+
+    }
+}
