258443edeca3bae80afec50f5babe67319fe894f
David Stevenson <drstevenson1958@yahoo.com>
2023-11-09 10:40:59 -0500

    Listing 9-14. The contents of the controller.js file

---
+++ ch09/ExampleApp/App/controllers/controller.js
@@ -0,0 +1,7 @@
+﻿myApp.controller("dayCtrl", function ($scope, days) {
+    $scope.day = days.today;
+});
+
+myApp.controller("tomorrowCtrl", function ($scope, days) {
+    $scope.day = days.tomorrow;
+});
